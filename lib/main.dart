8e02d8e0d2d24e1c3d74d53f621efcc7c9a08b55
Omar vargas <omarl.vargass@hotmail.com>
2023-06-22 17:46:39 -0600

    Curso flutter | Creando componente con estado

--- lib/main.dart
+++ lib/main.dart
@@ -103,6 +103,7 @@ class _InicioState extends State<Inicio> {
             buttonSection,
             textSection,
             textSection,
+            TapboxA(),
           ],
         ));
   }
@@ -176,6 +177,43 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  const TapboxA({super.key});
+
+  @override
+  State<TapboxA> createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
